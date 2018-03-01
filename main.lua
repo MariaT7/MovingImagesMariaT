90d4a4e7c0aaf37de930e5ad923ed482e74e5d4b
Maria T <maria.temu@ocsbstudent.ca>
2018-03-01 10:46:05 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,11 @@
+-- Title: MovingImages
+-- Name: Maria T
+-- Course: ICS2O/3C
+-- This program displays a beetle ship moving 
+-- across the screen as well as another object 
+-- moving in a different direction
+
+print ("***Testing beetleship!")
+
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
