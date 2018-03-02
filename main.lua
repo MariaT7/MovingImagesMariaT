0a5e1aad465cbc7912985d434ea3f5a0e5db765e
Maria T <maria.temu@ocsbstudent.ca>
2018-03-02 11:06:05 -0500

    4r4

--- main.lua
+++ main.lua
@@ -9,3 +9,33 @@ print ("***Testing beetleship!")
 
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
+
+-- global variables
+scrollSpeed = 3
+
+-- background image with width and height
+local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+
+-- characheter image with width and height
+local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+
+-- set the image to be transparent
+beetleship.alpha = 0
+
+--set the initial x and y position of beetleship
+beetleship.x = 0
+beetleship.y = display.contentHeight/3
+
+-- Function: MoveShip
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: this function adds the scroll speed to the x-value of the ship
+local function MoveShip(event)
+	-- add the scroll speed to the x-value of the ship
+	beetleship.x = beetleship.x + scrollSpeed
+	-- change the transparency of the ship evry time is moves so that it fades out
+	beetleship.alpha = beetleship.alpha + 0.01
+end
+
+-- MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame", MoveShip)
